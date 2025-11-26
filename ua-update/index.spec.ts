9f13013b1f31f00bc0698d69633317b66b55054f
Axel Leroy <axeleroy@users.noreply.github.com>
2025-11-26 11:36:06 +0100

    chore(ci): refactor test string

--- ua-update/index.spec.ts
+++ ua-update/index.spec.ts
@@ -61,10 +61,10 @@ describe("replaceUaString", () => {
     });
     it("should return false and don't change the file if the UA string hasn't changed", async () => {
         // GIVEN
-        const originalData = `const CHROMEOS_UAS = "Mozilla/5.0 (X11; CrOS x86_64 16181.61.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.6998.198 Safari/537.36";`;
-        fs.writeFileSync("/src/contentScript.js", originalData);
         const uaString =
             "Mozilla/5.0 (X11; CrOS x86_64 16181.61.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.6998.198 Safari/537.36";
+        const originalData = `const CHROMEOS_UAS = "${uaString}";`;
+        fs.writeFileSync("/src/contentScript.js", originalData);
 
         // WHEN
         const result = await replaceUaString(uaString);
