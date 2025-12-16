f9637216808cac2692602da2d405391c877af8be
Axel Leroy <axeleroy@users.noreply.github.com>
2025-12-16 16:39:27 +0100

    chore: clean eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,8 +5,12 @@ import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-    { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
-    { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+    {
+        files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+        plugins: { js },
+        extends: ["js/recommended"],
+        languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    },
     tseslint.configs.recommended,
     eslintPluginPrettierRecommended,
 ]);
