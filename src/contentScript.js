39ba43f89d4567b7285db9cc78d0b31ec224adf3
Axel Leroy <axeleroy@users.noreply.github.com>
2026-01-28 01:38:04 +0000

    Updated User-Agent to Chrome 144

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,5 +1,5 @@
 // prettier-ignore
-const CHROMEOS_UAS = "Mozilla/5.0 (X11; CrOS x86_64 16463.20.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.7499.28 Safari/537.36";
+const CHROMEOS_UAS = "Mozilla/5.0 (X11; CrOS x86_64 16503.60.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.7559.108 Safari/537.36";
 
 const scriptTag = document.createElement("script");
 scriptTag.type = "text/javascript";
